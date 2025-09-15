93defe2fded394a4ab2378b4a4349d868d2c5734
Moultrie Dangerfield <moultriedanger@gmail.com>
2025-09-15 18:49:43 -0400

    added basic llm calling and database

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import os
 from dotenv import load_dotenv
 from langchain.chat_models import init_chat_model
+from flask import Flask, request
+from langchain_community.utilities import SQLDatabase
+from prompts import query_prompt_template
 
 load_dotenv()
 
@@ -11,3 +14,35 @@ llm = init_chat_model("gpt-4o-mini", model_provider="openai")
 response = llm.invoke("What is the capital of France?")
 
 print(response.content)
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+	print('\033[1;34m' + 'I can talk!' + '\033[0m')
+
+	return 'Hello. I\'m flask'
+
+@app.route("/ai", methods = ["POST"])
+def call_ai():
+    print("ai called")
+    json_response = request.json
+    query = json_response.get('query')
+    
+    llm_response = llm.invoke(query)
+    print(llm_response.content)
+
+    return llm_response.content
+
+@app.route("/students", methods= ["GET"])
+def get_all_students():
+	
+    db = SQLDatabase.from_uri("sqlite:///sql.db")
+    print(db.dialect)
+    print(db.get_usable_table_names())
+    result = db.run("SELECT * FROM student LIMIT 10;")
+
+    return result
+if __name__ == '__main__':
+	app.run(port=8000)
---
+++ prompts.py
@@ -0,0 +1,28 @@
+from langchain_core.prompts import ChatPromptTemplate
+
+system_message = """
+Given an input question, create a syntactically correct {dialect} query to
+run to help find the answer. Unless the user specifies in his question a
+specific number of examples they wish to obtain, always limit your query to
+at most {top_k} results. You can order the results by a relevant column to
+return the most interesting examples in the database.
+
+Never query for all the columns from a specific table, only ask for a the
+few relevant columns given the question.
+
+Pay attention to use only the column names that you can see in the schema
+description. Be careful to not query for columns that do not exist. Also,
+pay attention to which column is in which table.
+
+Only use the following tables:
+{table_info}
+"""
+
+user_prompt = "Question: {input}"
+
+query_prompt_template = ChatPromptTemplate(
+    [("system", system_message), ("user", user_prompt)]
+)
+
+for message in query_prompt_template.messages:
+    message.pretty_print()
